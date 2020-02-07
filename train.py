3951b1dd739aba67a3d7ffa5352ce16b158fce88
ahmed.nasr9677@gmail.com <ahmed.nasr9677@gmail.com>
2020-02-07 21:59:10 +0200

    update train.py to help training process

--- train.py
+++ train.py
@@ -18,10 +18,23 @@ from datagen import ListDataset
 
 from torch.autograd import Variable
 
+drive_link = "'/content/drive/My Drive/retina-weights'"
+training_log = 'training_log.txt'
 
 parser = argparse.ArgumentParser(description='PyTorch RetinaNet Training')
 parser.add_argument('--lr', default=1e-3, type=float, help='learning rate')
 parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
+parser.add_argument('--batchsize', default = 2, type = int)
+parser.add_argument('--classnum', default = 4, type = int)
+parser.add_argument('--epochsnum', default = 30, type = int)
+parser.add_argument('--datapath', type = str)
+parser.add_argument('--trainAnnotations', type = str)
+parser.add_argument('--valAnnotations', type = str)
+
+
+
+
+
 args = parser.parse_args()
 
 assert torch.cuda.is_available(), 'Error: CUDA not found!'
@@ -35,17 +48,26 @@ transform = transforms.Compose([
     transforms.Normalize((0.485,0.456,0.406), (0.229,0.224,0.225))
 ])
 
-trainset = ListDataset(root='/search/odin/liukuang/data/voc_all_images',
-                       list_file='./data/voc12_train.txt', train=True, transform=transform, input_size=600)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=16, shuffle=True, num_workers=8, collate_fn=trainset.collate_fn)
+data_path = args.datapath 
+train_annotations = args.trainAnnotations
+val_annotations = args.valAnnotations
+batch_size = args.batchsize
+num_classes = args.classnum
+epochs_num = args.epochsnum
 
-testset = ListDataset(root='/search/odin/liukuang/data/voc_all_images',
-                      list_file='./data/voc12_val.txt', train=False, transform=transform, input_size=600)
-testloader = torch.utils.data.DataLoader(testset, batch_size=16, shuffle=False, num_workers=8, collate_fn=testset.collate_fn)
+
+
+trainset = ListDataset(root=data_path,
+                       list_file=train_annotations, train=True, transform=transform, input_size=600)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=0, collate_fn=trainset.collate_fn)
+
+testset = ListDataset(root=data_path,
+                      list_file=val_annotations, train=False, transform=transform, input_size=600)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size // 2, shuffle=False, num_workers=0, collate_fn=testset.collate_fn)
 
 # Model
-net = RetinaNet()
-net.load_state_dict(torch.load('./model/net.pth'))
+net = RetinaNet(num_classes)
+net.load_state_dict(torch.load('net.pth'))
 if args.resume:
     print('==> Resuming from checkpoint..')
     checkpoint = torch.load('./checkpoint/ckpt.pth')
@@ -56,7 +78,8 @@ if args.resume:
 net = torch.nn.DataParallel(net, device_ids=range(torch.cuda.device_count()))
 net.cuda()
 
-criterion = FocalLoss()
+
+criterion = FocalLoss(num_classes)
 optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=1e-4)
 
 # Training
@@ -76,8 +99,8 @@ def train(epoch):
         loss.backward()
         optimizer.step()
 
-        train_loss += loss.data[0]
-        print('train_loss: %.3f | avg_loss: %.3f' % (loss.data[0], train_loss/(batch_idx+1)))
+        train_loss += loss.item()
+        print('train_loss: %.3f | avg_loss: %.3f' % (loss.item(), train_loss/(batch_idx+1)))
 
 # Test
 def test(epoch):
@@ -91,8 +114,8 @@ def test(epoch):
 
         loc_preds, cls_preds = net(inputs)
         loss = criterion(loc_preds, loc_targets, cls_preds, cls_targets)
-        test_loss += loss.data[0]
-        print('test_loss: %.3f | avg_loss: %.3f' % (loss.data[0], test_loss/(batch_idx+1)))
+        test_loss += loss.item()
+        print('test_loss: %.3f | avg_loss: %.3f' % (loss.item(), test_loss/(batch_idx+1)))
 
     # Save checkpoint
     global best_loss
@@ -108,8 +131,21 @@ def test(epoch):
             os.mkdir('checkpoint')
         torch.save(state, './checkpoint/ckpt.pth')
         best_loss = test_loss
+        cmd = "cp './checkpoint/ckpt.pth' " + drive_link
+        os.system(cmd)
 
+        with open(training_log, 'a') as f:
+          f.write(str(epoch) + "  :  " + str(test_loss) + '\n')
 
-for epoch in range(start_epoch, start_epoch+200):
+        fcmd = "cp " + training_log + " " + drive_link
+        os.system(fcmd)
+
+        
+
+print('training starts with: ' + str(epochs_num) + ' epochs..')
+
+for epoch in range(start_epoch, start_epoch+epochs_num):
     train(epoch)
     test(epoch)
+
+
